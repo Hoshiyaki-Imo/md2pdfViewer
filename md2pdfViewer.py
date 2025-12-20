0eefac85b142f135381fa13721672c2febc8182b
HoshiYakiImo <watatyan0729@gmail.com>
2025-12-20 21:26:35 +0900

    first commit

---
+++ md2pdfViewer.py
@@ -0,0 +1,189 @@
+import PySide6
+from PySide6.QtWidgets import *
+from PySide6.QtGui import *
+from PySide6.QtCore import *
+from PySide6.QtWebEngineWidgets import QWebEngineView
+import os
+import sys
+import markdown
+import chardet
+
+# PySide6のアプリ本体（ユーザがコーディングしていく部分）
+class MainWindow(QMainWindow):
+    def __init__(self, parent=None):
+        # 親クラスの初期化
+        super().__init__(parent)
+        self.ChangedList = {}
+        self.settings = QSettings("HoshiYakiImo", "md2pdfViewer")
+
+        self.NowSetting = {"Change__ChangeTool" : self.settings.value("Change/ChangeTool", "weasyprint"),
+                           "Change__ChangeCompletedDialog" : self.settings.value("Change/ChangeCompletedDialog", True, type = bool)
+                           }
+
+        self.setWindowTitle(self.tr("md2pdfViewer"))
+        self.resize(800,600)
+        self.makeMenuBar()
+        MainLayout = QVBoxLayout()
+
+        self.Preview = QWebEngineView()
+        MainLayout.addWidget(self.Preview)
+        self.ChangeButton = QPushButton(self.tr("変換"))
+        self.ChangeButton.clicked.connect(self.Change)
+        self.ChangeButton.setDisabled(True)
+        FileChooseLayout = QHBoxLayout()
+        self.FileChooseButton = QPushButton(self.tr("ファイルを選択"))
+        self.FileChooseButton.clicked.connect(self.file_choose)
+        self.FileName = QLabel("")
+        
+        FileChooseLayout.addWidget(self.FileName)
+        FileChooseLayout.addWidget(self.FileChooseButton)
+
+        MainLayout.addLayout(FileChooseLayout)
+        MainLayout.addWidget(self.ChangeButton)
+        centralWidget = QWidget()
+        centralWidget.setLayout(MainLayout)
+        self.setCentralWidget(centralWidget)
+        self.StatusBar = self.statusBar()
+        self.setStatusBar(self.StatusBar)
+
+    def makeMenuBar(self):
+        #メニューバーの作成
+        MenuBar = self.menuBar()
+        mFile = MenuBar.addMenu(self.tr("ファイル"))
+        mEdit = MenuBar.addMenu(self.tr("編集"))
+        mHelp = MenuBar.addMenu(self.tr("ヘルプ"))
+
+
+        acSetting = QAction(self.tr("設定"),self)
+        acSetting.triggered.connect(self.SettingDialog)
+
+        #メニューバーにアクションを追加
+
+        mEdit.addAction(acSetting)
+
+    def file_choose(self):
+        self.filename, tmp = QFileDialog.getOpenFileName(self,self.tr("ファイルを開く"),"","Text File (*.html *.htm *.md)")
+        self.FileName.setText(str(self.filename))
+        with open (self.filename,"rb") as f:
+            tmp = f.read()
+            result = chardet.detect(tmp)
+        if result["encoding"] == "SHIFT_JIS":
+            self.encoding = "CP932"
+        else:
+            self.encoding = result["encoding"]
+        with open(self.filename,"r", encoding = self.encoding) as f:
+            convert_text = f.read()
+        extension = os.path.splitext(self.filename)[1]
+        if extension == ".md":
+            md = markdown.Markdown()
+            self.HTML_text = md.convert(convert_text)
+        elif extension == ".html" or extension == ".htm":
+            self.HTML_text = convert_text
+        self.ChangeButton.setDisabled(False)
+        if(self.NowSetting["Change__ChangeTool"] == "xhtml2pdf"):
+            #self.HTML_text = self.HTML_text[:self.HTML_text.find("<html>")+6] + "\n@font-face {font-family: \"my_lang\"; src: url(" + ");}" html, body {font-family: "my_lang";}"
+            pass
+        self.Preview.setHtml(self.HTML_text)
+
+    def Change(self):
+        output_filename = os.path.splitext(self.filename)[0] + ".pdf"
+        if os.path.isfile(output_filename):
+            
+        if(self.NowSetting["Change__ChangeTool"] == "weasyprint"):
+            from weasyprint import HTML
+            HTML(string = self.HTML_text).write_pdf(output_filename)
+        elif(self.NowSetting["Change__ChangeTool"] == "xhtml2pdf"):
+            from xhtml2pdf import pisa
+            with open(output_filename, "w+b") as file:
+                pisa_status = pisa.CreatePDF(src=self.HTML_text, dest=file)
+        if os.path.isfile(output_filename) and self.NowSetting["Change__ChangeCompletedDialog"]:
+            QMessageBox.information(self, self.tr("変換成功"), self.tr("ファイルの変換に成功しました"))
+        elif not os.path.isfile(output_filename):
+            QMessageBox.warning(self, self.tr("エラー"), self.tr("変換時にエラーが発生しました"))
+
+
+    def SettingDialog(self):
+        sd = SettingDialog()
+        sd.exec()
+
+class SettingDialog(QDialog):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle(self.tr("設定"))
+        mainlayout = QVBoxLayout()
+        abovelayout = QHBoxLayout()
+
+        self.stab = QListWidget()
+        self.stab.addItems([self.tr("変換"),self.tr("表示")])
+        self.stab.itemClicked.connect(self.stabChanged)
+        self.stab.setSizePolicy(QSizePolicy.Maximum, QSizePolicy.Preferred)
+        self.stab.setFixedWidth(80)
+        self.sdetail = QStackedWidget()
+        self.sdetail.addWidget(self.wrap_scroll(Setting_Change()))
+        self.sdetail.addWidget(self.wrap_scroll(Setting_Display()))
+
+        self.save_button = QPushButton(self.tr("設定を反映"))
+        self.save_button.clicked.connect(self.SaveSetting)
+
+        self.stab.currentRowChanged.connect(self.sdetail.setCurrentIndex)
+        self.stab.setCurrentRow(0)
+
+
+        self.setLayout(mainlayout)
+        mainlayout.addLayout(abovelayout)
+        mainlayout.addWidget(self.save_button)
+        abovelayout.addWidget(self.stab,1)
+        abovelayout.addWidget(self.sdetail)
+
+    def stabChanged(self):
+        pass
+
+    def wrap_scroll(self, widget : QWidget):
+        scroll = QScrollArea()
+        scroll.setWidget(widget)
+        scroll.setWidgetResizable(True)
+        return scroll
+    
+    def SaveSetting(self):
+        print(window.ChangedList)
+        if window.ChangedList == window.NowSetting:
+            pass
+        else:
+            for key, value in window.ChangedList.items():
+                window.settings.setValue(key.replace("__","/"),str(value))
+                window.NowSetting[key] = value
+
+
+class Setting_Change(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.SCLayout = QFormLayout()
+        self.setLayout(self.SCLayout)
+
+        ChangeTool = QComboBox()
+        ChangeTool.addItems(["weasyprint","xhtml2pdf"])
+        ChangeTool.setCurrentText(window.NowSetting["Change__ChangeTool"])
+        ChangeTool.currentIndexChanged.connect(lambda _ : (window.ChangedList.update(Change__ChangeTool = ChangeTool.currentText())))
+        ChangeCompletedDialog = QCheckBox(self.tr("表示する"))
+        ChangeCompletedDialog.setChecked(window.NowSetting["Change__ChangeCompletedDialog"])
+        ChangeCompletedDialog.stateChanged.connect(lambda _ : (window.ChangedList.update(Change__ChangeCompletedDialog = ChangeCompletedDialog.isChecked())))
+
+        self.SCLayout.addRow(self.tr("変換ツール"), ChangeTool)
+        self.SCLayout.addRow(self.tr("変換完了通知"), ChangeCompletedDialog)
+
+class Setting_Display(QWidget):
+    def __init__(self):
+        super().__init__()
+
+
+if __name__ == "__main__":
+    # 環境変数にPySide6を登録
+    dirname = os.path.dirname(PySide6.__file__)
+    plugin_path = os.path.join(dirname, 'plugins', 'platforms')
+    os.environ['QT_QPA_PLATFORM_PLUGIN_PATH'] = plugin_path
+
+    app = QApplication(sys.argv)    # PySide6の実行
+    window = MainWindow()           # ユーザがコーディングしたクラス
+    window.show()                   # PySide6のウィンドウを表示
+    sys.exit(app.exec())            # PySide6の終了
