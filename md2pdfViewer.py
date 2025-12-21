e5d9593b413fe358672dd68d76b22e57c998714c
HoshiYakiImo <watatyan0729@gmail.com>
2025-12-21 13:26:06 +0900

    v0.4.0 Settingクラスを別に設け、細かなエラーを修正

--- md2pdfViewer.py
+++ md2pdfViewer.py
@@ -86,7 +86,7 @@ class MainWindow(QMainWindow):
 
     def file_choose(self):
         self.filename, tmp = QFileDialog.getOpenFileName(self,self.tr("ファイルを開く"),"","Text File (*.html *.htm *.md)")
-        self.FileName.setText(str(self.filename))
+        self.StatusBar.showMessage(self.tr("ファイルを開きました : " + str(self.filename)))
         with open (self.filename,"rb") as f:
             tmp = f.read()
             result = chardet.detect(tmp)
@@ -127,7 +127,8 @@ class MainWindow(QMainWindow):
 
 
     def SettingDialog(self):
-        sd = SettingDialog(self)
+        import md2pdf_Setting as s
+        sd = s.SettingDialog(self)
         sd.exec()
 
     def versionInfo(self):
@@ -168,72 +169,10 @@ class MainWindow(QMainWindow):
         self.ChangeButton.setDisabled(False)
         self.acClose.setDisabled(False)
 
-class SettingDialog(QDialog):
-    def __init__(self,mainwin):
-        super().__init__()
-        self.mainwin = mainwin
-
-        self.setWindowTitle(self.tr("設定"))
-        mainlayout = QVBoxLayout()
-        abovelayout = QHBoxLayout()
-
-        self.stab = QListWidget()
-        self.stab.addItems([self.tr("変換"),self.tr("表示")])
-        self.stab.setSizePolicy(QSizePolicy.Maximum, QSizePolicy.Preferred)
-        self.stab.setFixedWidth(80)
-        self.sdetail = QStackedWidget()
-        self.sdetail.addWidget(self.wrap_scroll(Setting_Change(self.mainwin)))
-        self.sdetail.addWidget(self.wrap_scroll(Setting_Display()))
-
-        self.save_button = QPushButton(self.tr("設定を反映"))
-        self.save_button.clicked.connect(self.SaveSetting)
-
-        self.stab.currentRowChanged.connect(self.sdetail.setCurrentIndex)
-        self.stab.setCurrentRow(0)
-
-
-        self.setLayout(mainlayout)
-        mainlayout.addLayout(abovelayout)
-        mainlayout.addWidget(self.save_button)
-        abovelayout.addWidget(self.stab,1)
-        abovelayout.addWidget(self.sdetail)
 
 
 
-    def wrap_scroll(self, widget : QWidget):
-        scroll = QScrollArea()
-        scroll.setWidget(widget)
-        scroll.setWidgetResizable(True)
-        return scroll
 
-    def SaveSetting(self):
-        if self.mainwin.BeingEditedList == self.mainwin.ConfirmedSetting:
-            QMessageBox.information(self,self.tr("一応通知"), self.tr("設定は変更されていません"))
-        else:
-            self.mainwin.ChangeSetting()
-
-
-class Setting_Change(QWidget):
-    def __init__(self, mainwin):
-        super().__init__()
-        self.mainwin = mainwin
-        self.SCLayout = QFormLayout()
-        self.setLayout(self.SCLayout)
-
-        ChangeTool = QComboBox()
-        ChangeTool.addItems(["weasyprint","xhtml2pdf"])
-        ChangeTool.setCurrentText(self.mainwin.ConfirmedSetting["Change__ChangeTool"])
-        ChangeTool.currentIndexChanged.connect(lambda _ : (self.mainwin.BeingEditedList.update(Change__ChangeTool = ChangeTool.currentText())))
-        ChangeCompletedDialog = QCheckBox(self.tr("表示する"))
-        ChangeCompletedDialog.setChecked(self.mainwin.ConfirmedSetting["Change__ChangeCompletedDialog"])
-        ChangeCompletedDialog.stateChanged.connect(lambda _ : (self.mainwin.BeingEditedList.update(Change__ChangeCompletedDialog = ChangeCompletedDialog.isChecked())))
-
-        self.SCLayout.addRow(self.tr("変換ツール"), ChangeTool)
-        self.SCLayout.addRow(self.tr("変換完了通知"), ChangeCompletedDialog)
-
-class Setting_Display(QWidget):
-    def __init__(self):
-        super().__init__()
 
 class InformationDialog(QDialog):
     def __init__(self, wintitle = "Title", wincontent = "Content", width = 200, height = 100):
---
+++ md2pdf_Setting.py
@@ -0,0 +1,68 @@
+from PySide6.QtWidgets import *
+class SettingDialog(QDialog):
+    def __init__(self,mainwin):
+        super().__init__()
+        self.mainwin = mainwin
+        import md2pdf_Setting
+
+        self.setWindowTitle(self.tr("設定"))
+        mainlayout = QVBoxLayout()
+        abovelayout = QHBoxLayout()
+
+        self.stab = QListWidget()
+        self.stab.addItems([self.tr("変換"),self.tr("表示")])
+        self.stab.setSizePolicy(QSizePolicy.Maximum, QSizePolicy.Preferred)
+        self.stab.setFixedWidth(80)
+        self.sdetail = QStackedWidget()
+        self.sdetail.addWidget(self.wrap_scroll(Setting_Change(self.mainwin)))
+        self.sdetail.addWidget(self.wrap_scroll(Setting_Display()))
+
+        self.save_button = QPushButton(self.tr("設定を反映"))
+        self.save_button.clicked.connect(self.SaveSetting)
+
+        self.stab.currentRowChanged.connect(self.sdetail.setCurrentIndex)
+        self.stab.setCurrentRow(0)
+
+
+        self.setLayout(mainlayout)
+        mainlayout.addLayout(abovelayout)
+        mainlayout.addWidget(self.save_button)
+        abovelayout.addWidget(self.stab,1)
+        abovelayout.addWidget(self.sdetail)
+
+
+
+    def wrap_scroll(self, widget : QWidget):
+        scroll = QScrollArea()
+        scroll.setWidget(widget)
+        scroll.setWidgetResizable(True)
+        return scroll
+
+    def SaveSetting(self):
+        if self.mainwin.BeingEditedList == self.mainwin.ConfirmedSetting:
+            QMessageBox.information(self,self.tr("一応通知"), self.tr("設定は変更されていません"))
+        else:
+            self.mainwin.ChangeSetting()
+
+            
+class Setting_Change(QWidget):
+    def __init__(self, mainwin):
+        super().__init__()
+        self.mainwin = mainwin
+        self.SCLayout = QFormLayout()
+        self.setLayout(self.SCLayout)
+
+        ChangeTool = QComboBox()
+        ChangeTool.addItems(["weasyprint","xhtml2pdf"])
+        ChangeTool.setCurrentText(self.mainwin.ConfirmedSetting["Change__ChangeTool"])
+        ChangeTool.currentIndexChanged.connect(lambda _ : (self.mainwin.BeingEditedList.update(Change__ChangeTool = ChangeTool.currentText())))
+        ChangeCompletedDialog = QCheckBox(self.tr("表示する"))
+        ChangeCompletedDialog.setChecked(self.mainwin.ConfirmedSetting["Change__ChangeCompletedDialog"])
+        ChangeCompletedDialog.stateChanged.connect(lambda _ : (self.mainwin.BeingEditedList.update(Change__ChangeCompletedDialog = ChangeCompletedDialog.isChecked())))
+
+        self.SCLayout.addRow(self.tr("変換ツール"), ChangeTool)
+        self.SCLayout.addRow(self.tr("変換完了通知"), ChangeCompletedDialog)
+
+class Setting_Display(QWidget):
+    def __init__(self):
+        super().__init__()
