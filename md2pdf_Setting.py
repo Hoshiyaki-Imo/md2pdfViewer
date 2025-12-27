67d2401f60aafc64d29e27224c00594dd243367b
Hoshiyaki-Imo <watatyan0729@gmail.com>
2025-12-27 14:42:47 +0900

    v0.6.0 mainにマージ、スペース挿入

--- md2pdf_Setting.py
+++ md2pdf_Setting.py
@@ -52,7 +52,7 @@ class Setting_Change(QWidget):
         self.setLayout(self.SCLayout)
 
         ChangeTool = QComboBox()
-        ChangeTool.addItems(["weasyprint","xhtml2pdf", "ChromiumPrint"])
+        ChangeTool.addItems(["weasyprint", "xhtml2pdf", "ChromiumPrint"])
         ChangeTool.setCurrentText(self.mainwin.ConfirmedSetting["Change__ChangeTool"])
         ChangeTool.currentIndexChanged.connect(lambda _ : (self.mainwin.BeingEditedList.update(Change__ChangeTool = ChangeTool.currentText())))
         ChangeCompletedDialog = QCheckBox(self.tr("表示する"))
